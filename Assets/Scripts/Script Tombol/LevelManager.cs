5743bef44b79ca5dbc54ca4d07da87420633a93b
Fariz Maulana Tamim <88298399+FarizMaulanaT@users.noreply.github.com>
2022-04-09 22:30:51 +0700

    cek

--- Assets/Scripts/Script Tombol/LevelManager.cs
+++ Assets/Scripts/Script Tombol/LevelManager.cs
@@ -9,12 +9,14 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject LevelCanvas;
     public GameObject Story1;
+    public GameObject Prolog;
  
     // Start is called before the first frame update
     void Start()
     {
         LevelCanvas.SetActive(true);
         Story1.SetActive(false);
+        Prolog.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,11 +28,20 @@ public class LevelManager : MonoBehaviour
     {
         LevelCanvas.SetActive(true);
         Story1.SetActive(false);
+        Prolog.SetActive(false);
+    }
+    public void PrologAwal()
+    {
+        Prolog.SetActive(true);
+        LevelCanvas.SetActive(false);
+        Story1.SetActive(false);
+
     }
     public void Storysatu()
     {
         LevelCanvas.SetActive(false);
         Story1.SetActive(true);
+        Prolog.SetActive(false);
     }
     public void MulaiSatu()
     {
