ed8871527725d0fac0de98ca1cca9fa6f4f6cca3
SeninbinKamis <88298482+SeninbinKamis@users.noreply.github.com>
2022-05-22 21:18:20 +0700

    Revert "coba"
    This reverts commit 7daddaa6dd57daaf7b0520f5e4e418ef7ceba86c.

--- Assets/Scripts/Script Tombol/RenderParticlesEffect.cs
+++ Assets/Scripts/Script Tombol/RenderParticlesEffect.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Camera))]
 public class RenderParticlesEffect : MonoBehaviour
@@ -14,7 +13,7 @@ public class RenderParticlesEffect : MonoBehaviour
     [SerializeField] private Vector2Int imageResolution = new Vector2Int(256, 256);
 
     // Reference the RawImage in your UI
-    [SerializeField] private RawImage targetImage1, targetImage2, targetImage3, targetImage4;
+    [SerializeField] private RawImage targetImage;
 
     private RenderTexture renderTexture;
 
@@ -25,13 +24,7 @@ public class RenderParticlesEffect : MonoBehaviour
         renderTexture = new RenderTexture(imageResolution.x, imageResolution.y, 32);
         particlesCamera.targetTexture = renderTexture;
 
-        targetImage1.texture = renderTexture;
-        if (SceneManager.GetActiveScene().name == "LevelSelection")
-        {
-            targetImage2.texture = renderTexture;
-            targetImage3.texture = renderTexture;
-            targetImage4.texture = renderTexture;
-        }
-        // targetImagePeng.texture = renderTexture;
+        targetImage.texture = renderTexture;
+       // targetImagePeng.texture = renderTexture;
     }
 }
